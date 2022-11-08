59b6cf9500fdfe931158b3f4a7aeec53f47354d3
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2022-11-07 21:52:23 -0500

    more code added and README update

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Quiz-of-doom
 quiz project for coding bootcamp
+<!-- GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score -->
+
+This project was brought on as an assignment for module 4 in our coding boot camp. This project could have been used for a review if the user wanted to use it that way. 
+
+I built this project as another step in my journey to become a senior software engineer. 
+
+This project could solve the problem of not having a way to review question having to do with our coding bootcamp. Although at this time, I found this project added more problems than it solved. 
+
+ Learning is the number 1 goal in the class and this project forced me to learn more about vanilla JavaScript. I have a lot to learn but every time I sat down to code on this project I learned more. Obviously I still have a lot to learn, but learning is the journey!
+
+ My project stands out! I am not sure it does in a good way YET but I will continue to work on it and turn it into the grade A project I know it can be. 
+
+The features of this project are a timer that will index down. It has a start button that works and it has a text box that doesn't look great YET! 
+
+I named this project the Quiz-of-doom because it seems that way right now but I have the oppertunity to master vanilla javascript. When I finish this project to my standards I will rename it appriopriatly. 
+
+
--- javascript.js
+++ javascript.js
@@ -3,11 +3,23 @@ var questions = document.querySelector('.question-area');
 var timerElement = document.querySelector('.timer-count');
 var startButton = document.querySelector('.start-button');
 var questionText = document.querySelector('.question-text');
-var choices = document.querySelector('.choices');
+var choicesDiv = document.querySelector('.choices');
 //var buttons = document.querySelector(".buttons")
 
+// var array = ['a','b', 'c', 'd']
+// array[2]
+
+// var object = {
+//     name: 'Patrick',
+//     age: 20,
+//     height: '6ft'
+// }
+// object.age
+
+
+
 var question = [
-  
+
     {
         prompt: "What is 1+1",
         choices: {
@@ -16,7 +28,7 @@ var question = [
             C: 3,
             D: 4
         },
-        answer: 2
+        answer: "D. 2"
     },
     {
         prompt: "What is 1+2",
@@ -26,41 +38,41 @@ var question = [
             C: 3,
             D: 4
         },
-        answer: 3
+        answer: "C. 3"
     },
     {
-        prompt:"what is 1+5",
-        choices:{
+        prompt: "what is 1+5",
+        choices: {
             A: 1,
             B: 2,
             C: 3,
-            D: 4
+            D: 6
         },
-        answer: 3
-        
+        answer: "D. 6"
+
     },
     {
-        prompt:"what is 1+5",
-        choices:{
+        prompt: "what is 1+5",
+        choices: {
             A: 1,
             B: 2,
             C: 3,
-            D: 4
+            D: 6
         },
-        answer: 3
-        
+        answer: "D. 6"
+
     }
-   //
+    //
 ]
 
-questionText.textContent = question[0].prompt 
+questionText.textContent = question[0].prompt
 
 
-    var buttons = document.createElement('button');
-    buttons.addEventListener ("click", function (displayQuestions){
-     document.body.appendChild()
-    }
- //What is 1+1
+var buttons = document.createElement('button');
+buttons.addEventListener("click", function (displayQuestions) {
+    document.body.appendChild()
+})
+//What is 1+1
 //Look throught the choices, for each choices: create a startButton, display text on that startButton, append button to hoices var
 
 
@@ -68,11 +80,11 @@ questionText.textContent = question[0].prompt
 question[0].prompt
 question[0].choices.C //Patrick
 question[0].answer
-questions[1].answer
+question[1].answer
 question[1].choices.C //3
-    
+
 var buttons = document.createElement('button')
-    
+
 
 // {
 //     var score: 0;
@@ -89,7 +101,7 @@ var buttons = document.createElement('button')
 //     alert(" you got" + score + "\" + questions.length)
 // }
 var timer;
-var timerCount = 60;
+var timerCount = 30;
 // var isCorrect = false;
 // var winCounter = 0;
 // var loseCounter = 0;
@@ -144,15 +156,49 @@ function startTimer() {
     }, 1000);
 }
 
-function displayQuestions() {
-    displayQuestions.textContent="question"
-}
+var index = 0;
 
+function checkAnswer(event) {
+    console.log(event.target.innerText)
+    console.log(this.textContent)
+    var pickedChoice = this.textContent;
+    if (pickedChoice = "B.2")
+        text = " Great Job! ";
+      else
+        text = " Better Luck Next Time "
+    }
+    //if pickedChoice equal to answer of that question: show correct
+    //else if: show incorrect, substract time
+
+    //if last question: end game
+
+    //if not: index++, call displayQuestions()
+
+
+function displayQuestions() {
+    var choices = question[index].choices
+
+    questionText.textContent = question[index].prompt
+
+    for (var key in choices) {
+        if (choices.hasOwnProperty(key)) {
+            console.log(key + " -> " + choices[key]);
+
+            var button = document.createElement('button');
+            button.textContent = key + '. ' + choices[key]
+
+            choicesDiv.appendChild(button)
+
+            button.onclick = checkAnswer;
+        }
+    }
+
+}
 
 //when start click function happens present a  question 
 startButton.addEventListener('click', function () {
     startTimer();
-    displayQuestions();
+    //displayQuestions();
 })
 
 
